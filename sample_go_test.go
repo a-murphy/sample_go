eae2136b9f065eebc344da3f0f3226290fee4ee3
a-murphy <a-murphy@users.noreply.github.com>
2015-03-12 18:05:54 -0700

    Update sample_go_test.go

--- sample_go_test.go
+++ sample_go_test.go
@@ -7,10 +7,10 @@ import (
 
 var _ = Describe("Sample", func() {
 	Describe("Calculating square root", func() {
-		Context("of the number 10", func() {
+		Context("of the number 4", func() {
 			It("should result 2", func() {
 				Setup()
-				Expect(GetResult()).To(Equal(2.0))
+				Expect(GetResult()).To(Equal(0))
 			})
 		})
 	})
